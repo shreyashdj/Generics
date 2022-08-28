6ac1634d66887562d0ebda42771351bc9912dc10
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-28 22:30:09 +0530

    Added:[Shreyash] code to print welcome message

---
+++ src/com/bridgelabz/generics/FindMaximumMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.generics;
+
+public class FindMaximumMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Find Maximum Program");
+		
+	}
+
+}
