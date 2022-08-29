6bf8ea511be70a8dfe985c1f6869587a9abb7f12
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-29 10:19:06 +0530

    [Shreyash]:Added code to add maximum method to find maximum Float value

--- src/com/bridgelabz/generics/FindMaximum.java
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -11,15 +11,34 @@ public class FindMaximum {
 		
 		Integer max = a;
 		
-		if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
+		if (b.compareTo(max) > 0) {
 			
 			max = b;
 		
-		else if (c.compareTo(max) > 0)
+		} else if (c.compareTo(max) > 0) {
 			
 			max = c;
+		}
 		
 		return max;
 	}
-
+	
+	// created maximum method to find maximum Float using compareTo
+	
+	public Float maximum(Float a, Float b, Float c) {
+		
+		Float max = a;
+		
+		if (b.compareTo(max) > 0) {
+			
+			max = b;
+		
+		} else if (c.compareTo(max) > 0) {
+			
+			max = c;
+		}
+		
+		return max;
+	}
+	
 }
