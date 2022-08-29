abf1118822e874412345b9b4219bff8a5c460a95
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-29 11:18:55 +0530

    [Shreyash]:Refactored code to find maximum using Generic method

--- src/com/bridgelabz/generics/FindMaximumMain.java
+++ src/com/bridgelabz/generics/FindMaximumMain.java
@@ -11,6 +11,8 @@ public class FindMaximumMain {
 		
 		FindMaximum findMaximum = new FindMaximum();		// created object of FindMaximum class
 		
+		//	printing maximum Integers, Floats and Strings
+		
 		System.out.println("\n Maximun Integer value is : " + findMaximum.maximum(56, 91, 22));
 		
 		System.out.println("\n Maximun Float value is : " + findMaximum.maximum(34.32f, 6.34f, 87.21f));
