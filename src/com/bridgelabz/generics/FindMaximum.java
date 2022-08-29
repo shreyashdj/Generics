0cb0ca395a5ef8a95ab242e5d9205dd50cfb4b95
shreyashdj <shreyashjadhav1799@gmail.com>
2022-08-29 10:31:08 +0530

    [Shreyash]:Added code to add maximum method to find maximum String

--- src/com/bridgelabz/generics/FindMaximum.java
+++ src/com/bridgelabz/generics/FindMaximum.java
@@ -41,4 +41,22 @@ public class FindMaximum {
 		return max;
 	}
 	
+	// created maximum method to find maximum String using compareTo
+	
+	public String maximum(String a, String b, String c) {
+			
+		String max = a;
+			
+			if (b.compareTo(max) > 0) {
+				
+				max = b;
+			
+			} else if (c.compareTo(max) > 0) {
+				
+				max = c;
+			}
+			
+			return max;
+		}
+	
 }
